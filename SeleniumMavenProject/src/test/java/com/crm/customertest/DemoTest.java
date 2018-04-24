c06f8b742ca686c211630882b9da0cf80a43ff2e
TYSS <TYSS@TYSS-TR-001>
2018-04-24 17:19:14 +0530

    DemoTest

---
+++ SeleniumMavenProject/src/test/java/com/crm/customertest/DemoTest.java
@@ -0,0 +1,12 @@
+package com.crm.customertest;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void testDemo()
+	{
+		System.out.println("test demo. . .");
+	}
+}

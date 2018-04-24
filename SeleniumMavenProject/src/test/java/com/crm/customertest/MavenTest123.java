d5193f94e1f14914a0add6fca23c546264f95796
TYSS <TYSS@TYSS-TR-0015>
2018-04-24 17:36:19 +0530

    Gargi

---
+++ SeleniumMavenProject/src/test/java/com/crm/customertest/MavenTest123.java
@@ -0,0 +1,12 @@
+package com.crm.customertest;
+
+import org.testng.annotations.Test;
+
+public class MavenTest123 {
+
+	@Test
+	public void abcTest123()
+	{
+		System.out.println("create test for maven");
+	}
+}
